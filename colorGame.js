3370542e6a3e05578d1a8468ac0bb6869998979f
Dariusz Okonski <darek200180@gmail.com>
2020-01-02 14:50:27 +0100

    display different colors and add eventListeners to each square

--- colorGame.js
+++ colorGame.js
@@ -1,1 +1,30 @@
-console.log('Hello world')
+let colors = [
+  'rgb(255, 0, 0)',
+  'rgb(255, 255, 0)',
+  'rgb(0, 255, 0)',
+  'rgb(0, 255, 255)',
+  'rgb(0, 0, 255)',
+  'rgb(255, 0, 255)'
+];
+
+let squares = document.querySelectorAll('.square');
+let pickedColor = colors[3];
+let colorcolorDisplay = document.getElementById('colorDisplay');
+
+colorcolorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  //add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+
+  squares[i].addEventListener('click', function () {
+    let clickedColor = this.style.backgroundColor;
+
+    if (clickedColor === pickedColor) {
+      console.log(clickedColor, pickedColor)
+      console.log('true')
+    } else {
+      console.log('false');
+    }
+  })
+}
